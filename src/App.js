4f1885461205bba376be2735d659a8ddacf14cd0
Himadri Purkait <purkaithimadricollege@gmail.com>
2023-10-03 22:49:53 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { RxCross2 } from "react-icons/rx";
 import { LiaMinusSolid } from "react-icons/lia";
 import { LiaPlusSolid } from "react-icons/lia";
 import { LiaEqualsSolid } from "react-icons/lia";
-import { FiDelete } from "react-icons/fi";
-import { useState } from "react";
+import { RiDeleteBack2Line } from "react-icons/ri";
+import { useEffect, useState } from "react";
 // import { LiaDivideSolid } from "react-icons/lia";
 
 function App() {
+  const [fastResult, setFastResult] = useState("");
+
   const [screen, setScreen] = useState("");
   const [subScreen, setSubScreen] = useState("");
 
@@ -18,7 +20,17 @@ function App() {
   const [numberTwo, setNumberTwo] = useState("");
 
   const [operator, setOperator] = useState("");
-  const [resultScreen, setResultScreen] = useState("r");
+  const [resultScreen, setResultScreen] = useState("");
+
+  useEffect(() => {
+    // fastresult();
+    // document.addEventListener("keydown", detectkeydown, true);
+  }, [screen]);
+
+  const detectkeydown = (e) => {
+    // console.log(e);
+    // setScreen(screen + e.key);
+  };
 
   function result() {
     if (operator === "1") {
@@ -40,34 +52,152 @@ function App() {
       setResultScreen(tempresult);
       console.log(resultScreen);
     }
+    //  else {
+    //   var tempresult = 0 + parseInt(screen);
+    //   setResultScreen(tempresult);
+    // }
   }
+
+  // function fastresult() {
+  //   var num1 = subScreen;
+  //   fastResult = fastresult;
+  // }
 
   return (
     <div className="font-bold bg-[#eeeef8] w-full h-[calc(100vh-60px)] lg:h-[100vh] md:h-[100vh] flex justify-center items-center">
-      <div className="w-full lg:w-[340px] md:w-[340px] h-[calc(100vh-60px)] lg:h-[calc(100vh-100px)] md:h-[calc(100vh-100px)]">
+      <div className="neomorphismin rounded-[30px] w-full lg:w-[340px] md:w-[340px] h-[calc(100vh-60px)] lg:h-[calc(100vh-100px)] md:h-[calc(100vh-100px)]">
         <div className=" h-[calc(100%-478px)] lg:h-[calc(100%-445px)] md:h-[calc(100%-445px)] w-full flex justify-center items-center px-[24px] lg:px-[16px] md:px-[16px] ">
-          <div className="neomorphismin w-full h-[calc(100%-50px)] rounded-3xl p-[20px] flex flex-col-reverse">
+          <div className="neomorphisminside w-full h-[calc(100%-50px)] rounded-3xl p-[20px] flex flex-col-reverse">
             {/* <span>{resultScreen}</span>{" "} */}
-            <input
+            {resultScreen.length === 0 ? (
+              <>
+                <div
+                  className="w-full h-[25px] flex justify-end items-center"
+                  style={{ transition: ".4s" }}
+                >
+                  {subScreen.length === 0 && operator.length === 0 ? (
+                    <></>
+                  ) : (
+                    <>
+                      <LiaEqualsSolid
+                        className="mr-[7px]  h-[25px] text-[14px] text-[#939393]"
+                        style={{ transition: ".4s" }}
+                      />
+                    </>
+                  )}
+                  <span
+                    className="bg-transparent w-auto min-w-0 max-w-[246px] h-full flex justify-end items-start overflow-hidden outline-none font-[timer2] font-thin select-none  text-[17px] text-right text-[#939393]"
+                    style={{ transition: ".4s" }}
+                  >
+                    {operator.length === 0 ? (
+                      <>{subScreen}</>
+                    ) : (
+                      <>
+                        {operator === "1" ? (
+                          <>
+                            {subScreen.length === 0 ? (
+                              <>{parseInt(numberOne) / parseInt("1")}</>
+                            ) : (
+                              <>{parseInt(numberOne) / parseInt(subScreen)}</>
+                            )}
+                          </>
+                        ) : operator === "2" ? (
+                          <>
+                            {subScreen.length === 0 ? (
+                              <>{parseInt(numberOne) * parseInt("1")}</>
+                            ) : (
+                              <>{parseInt(numberOne) * parseInt(subScreen)}</>
+                            )}
+                          </>
+                        ) : operator === "3" ? (
+                          <>
+                            {subScreen.length === 0 ? (
+                              <>{parseInt(numberOne) - parseInt("0")}</>
+                            ) : (
+                              <>{parseInt(numberOne) - parseInt(subScreen)}</>
+                            )}
+                          </>
+                        ) : (
+                          <>
+                            {subScreen.length === 0 ? (
+                              <>{parseInt(numberOne) + parseInt("0")}</>
+                            ) : (
+                              <>{parseInt(numberOne) + parseInt(subScreen)}</>
+                            )}
+                          </>
+                        )}
+                      </>
+                    )}
+                  </span>
+                </div>
+              </>
+            ) : (
+              <>
+                <div
+                  className="w-full h-[35px] flex justify-end items-center"
+                  style={{ transition: ".4s" }}
+                >
+                  <LiaEqualsSolid
+                    className="mr-[7px]  h-[35px] text-[17px]"
+                    style={{ transition: ".4s" }}
+                  />
+                  <span
+                    className="bg-transparent w-auto min-w-0 max-w-[246px] h-full flex justify-end items-start overflow-hidden outline-none font-[timer2] font-thin select-none  text-[22px] text-right"
+                    style={{ transition: ".4s" }}
+                  >
+                    {operator === "1" ? (
+                      <>{parseInt(numberOne) / parseInt(subScreen)}</>
+                    ) : operator === "2" ? (
+                      <>{parseInt(numberOne) * parseInt(subScreen)}</>
+                    ) : operator === "3" ? (
+                      <>{parseInt(numberOne) - parseInt(subScreen)}</>
+                    ) : (
+                      <>{parseInt(numberOne) + parseInt(subScreen)}</>
+                    )}
+                  </span>
+                </div>
+              </>
+            )}
+            {resultScreen.length != 0 ? (
+              <>
+                <input
+                  className="bg-transparent w-full h-[50px] outline-none font-[timer2] font-thin select-none  text-[17px] text-right text-[#939393]"
+                  style={{ transition: ".4s" }}
+                  value={screen}
+                  onChange={(e) => {
+                    setScreen(e.target.value);
+                    // fastresult();
+                  }}
+                ></input>
+              </>
+            ) : (
+              <>
+                <input
+                  className="bg-transparent w-full h-[60px] outline-none font-[timer2] font-thin select-none  text-[22px] text-right "
+                  style={{ transition: ".4s" }}
+                  value={screen}
+                  onKeyDown={(e) => setScreen(e)}
+                  onChange={(e) => {
+                    setScreen(e.target.value);
+                    // fastresult();
+                    if (operator.length === 1) {
+                      result();
+                    }
+                  }}
+                ></input>
+              </>
+            )}
+            {/* <input
               className="bg-transparent w-full h-[60px] outline-none font-[timer2] font-thin select-none  text-[22px] text-right"
-              value={resultScreen}
+              // value={subScreen}
               onChange={(e) => setScreen(e.target.value)}
             ></input>
             <input
               className="bg-transparent w-full h-[60px] outline-none font-[timer2] font-thin select-none  text-[22px] text-right"
-              value={screen}
+              // value={numberOne}
               onChange={(e) => setScreen(e.target.value)}
-            ></input>
-            <input
-              className="bg-transparent w-full h-[60px] outline-none font-[timer2] font-thin select-none  text-[22px] text-right"
-              value={subScreen}
-              onChange={(e) => setScreen(e.target.value)}
-            ></input>
-            <input
-              className="bg-transparent w-full h-[60px] outline-none font-[timer2] font-thin select-none  text-[22px] text-right"
-              value={numberOne}
-              onChange={(e) => setScreen(e.target.value)}
-            ></input>
+            ></input> */}
+            <div className="h-[120px] w-full "></div>
           </div>
         </div>
         <div className="w-full flex justify-center items-center">
@@ -81,6 +211,8 @@ function App() {
               onClick={() => {
                 setScreen("");
                 setOperator("");
+                setResultScreen("");
+                setSubScreen("");
               }}
             >
               C
@@ -90,7 +222,7 @@ function App() {
               style={{ transition: ".3s" }}
               onClick={() => setScreen(screen.slice(0, -1))}
             >
-              <FiDelete />
+              <RiDeleteBack2Line className="text-[20px]" />
             </div>
             <div
               className="neomorphism  w-[70px] lg:w-[65px] md:w-[65px] h-[70px] lg:h-[65px] md:h-[65px]  rounded-full flex justify-center items-center text-[22px] hover:bg-[#ffd18b] cursor-pointer bg-[#ffb545] select-none"
@@ -111,7 +243,7 @@ function App() {
                 }
               }}
             >
-              <LiaDivideSolid />
+              <LiaDivideSolid className="text-[20px]" />
             </div>
           </div>
           <div className="flex justify-evenly items-center my-[10px] font-[timer2] font-thin">
@@ -158,7 +290,7 @@ function App() {
                 }
               }}
             >
-              <RxCross2 />
+              <RxCross2 className="text-[20px]" />
             </div>
           </div>
           <div className="flex justify-evenly items-center my-[10px] font-[timer2] font-thin">
@@ -196,7 +328,7 @@ function App() {
               className="neomorphism  w-[70px] lg:w-[65px] md:w-[65px] h-[70px] lg:h-[65px] md:h-[65px]  rounded-full flex justify-center items-center cursor-pointer bg-[#ffb545] hover:bg-[#ffd18b] select-none"
               style={{ transition: ".3s" }}
               onClick={() => {
-                if (operator.length >= 3) {
+                if (operator.length >= 1) {
                 } else {
                   setOperator("3");
                   setNumberOne(subScreen);
@@ -205,7 +337,7 @@ function App() {
                 }
               }}
             >
-              <LiaMinusSolid />
+              <LiaMinusSolid className="text-[20px]" />
             </div>
           </div>
           <div className="flex justify-evenly items-center my-[10px] font-[timer2] font-thin">
@@ -243,7 +375,7 @@ function App() {
               className="neomorphism  w-[70px] lg:w-[65px] md:w-[65px] h-[70px] lg:h-[65px] md:h-[65px]  rounded-full flex justify-center items-center cursor-pointer bg-[#ffb545] hover:bg-[#ffd18b] select-none"
               style={{ transition: ".3s" }}
               onClick={() => {
-                if (operator.length >= 4) {
+                if (operator.length >= 1) {
                 } else {
                   setOperator("4");
                   setNumberOne(subScreen);
@@ -252,7 +384,7 @@ function App() {
                 }
               }}
             >
-              <LiaPlusSolid />
+              <LiaPlusSolid className="text-[20px]" />
             </div>
           </div>
           <div className="flex justify-evenly items-center my-[10px] font-[timer2] font-thin">
@@ -283,7 +415,7 @@ function App() {
               style={{ transition: ".3s" }}
               onClick={() => result()}
             >
-              <LiaEqualsSolid />
+              <LiaEqualsSolid className="text-[20px]" />
             </div>
           </div>
         </div>
